d272297abaedb17a2f32d1cd98f2e46f4aa6ae5a
TamasKerdo <kerdothamas@gmail.com>
2017-11-27 17:42:09 +0100

    feat(exercise 22 is done)

--- week-02/day1/exercises/exercise27/exercise27/exercise27/Program.cs
+++ week-02/day1/exercises/exercise27/exercise27/exercise27/Program.cs
@@ -11,16 +11,43 @@ namespace exercise27
         static void Main(string[] args)
         {
             double a = 24;
-            int out = 0;
+            int outt = 0;
+
             // if w is even increment out by one
 
-            Console.WriteLine(out);
+            string input = Console.ReadLine();
+            float w;
+            float.TryParse(input, out w);
+            w = w % 2;
+            if (w == 0)
+            {
+               outt++;
+            }
+
+            Console.WriteLine(outt);
 
             int b = 13;
             string out2 = "";
             // if b is between 10 and 20 set out2 to "Sweet!"
             // if less than 10 set out2 to "More!",
             // if more than 20 set out2 to "Less!"
+
+            if (b > 10 && b < 20)
+            {
+                out2 = "Sweet!";
+            }
+            else if (b < 10)
+            {
+                out2 = "More!";
+            }
+            else if (b > 20)
+            {
+                out2 = "Less!";
+            }
+
+
+
+
 
             Console.WriteLine(out2);
 
@@ -33,6 +60,21 @@ namespace exercise27
             // and isBonus is false decrement c by 1
             // if isBonus is true c should remain the same
 
+
+            if ((credits >= 50) && (isBonus == false))
+            {
+                c--;
+                c--;
+            }
+
+            else if ((credits < 50) && (isBonus == false))
+            {
+                c--;
+            }
+
+           
+           
+
             Console.WriteLine(c);
 
             int d = 8;
@@ -44,6 +86,24 @@ namespace exercise27
             // if time is more than 200
             // set out3 to "Time out"
             // otherwise set out3 to "Run Forest Run!"
+            float floatd = d;
+            
+            floatd = floatd % 4;
+
+            if ((floatd == 0) && (time <= 200))
+            {
+                out3 = "check";
+            }
+
+            else if (time > 200)
+            {
+                out3 = "Time out";
+            }
+
+            else
+            {
+                out3 = "Run Forest Run!";
+            }
 
             Console.WriteLine(out3);
             }
