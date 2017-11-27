c6613ab5b3cebd814b1009cf479007895763ab3b
TamasKerdo <kerdothamas@gmail.com>
2017-11-27 15:27:54 +0100

    04 05 exercise is done

---
+++ week-02/day1/exercise04/exercise04/exercise04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            //  - Your name
+            //  - Your age
+            //  - Your height in meters (as a decimal fraction)
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+
+            Console.WriteLine("Tamas");
+            Console.WriteLine("Iam 29");
+            Console.WriteLine("Iam 190 m tall");
+
+        }
+    }
+}
