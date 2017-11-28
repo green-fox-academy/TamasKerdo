b527b87fb2ea4e9d13008f434742f9b9073e0374
TamasKerdo <kerdothamas@gmail.com>
2017-11-28 12:02:50 +0100

    feat(Greeter_function is done)

---
+++ week-02/day2/Append_a/Append_a/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Append_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+
+            string am = "kuty";
+
+            Console.WriteLine(appendA(am));
+            Console.ReadLine();
+        }
+
+        static string appendA(string am)
+        {
+            int stringLenght = am.Length;
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(am);
+            sb.Insert(stringLenght, "a");
+            string str1 = sb.ToString();
+
+            return str1;
+        }
+    }
+}
