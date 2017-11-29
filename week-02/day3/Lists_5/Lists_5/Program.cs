0fa84b1c8321ba7119899d115b4f9da0d241de15
TamasKerdo <kerdothamas@gmail.com>
2017-11-29 16:29:18 +0100

    feat(Lists_5 is done)

--- week-02/day3/Lists_5/Lists_5/Program.cs
+++ week-02/day3/Lists_5/Lists_5/Program.cs
@@ -10,6 +10,39 @@ namespace Lists_5
     {
         static void Main(string[] args)
         {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+
+
+            //Solution 1
+
+
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].Equals(7))
+                {
+                    Console.WriteLine("Hooray");
+                }
+                else if(i==List.Count-1)
+                {
+                    Console.WriteLine("Nooooooo");
+                }
+            }
+
+            //Solution 2
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hooray");
+            }
+            else
+            {
+                Console.WriteLine("Nooooooo");
+            }
+
+            Console.ReadLine();
         }
     }
 }
