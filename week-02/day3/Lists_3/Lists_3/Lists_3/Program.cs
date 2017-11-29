403f4b99b9d5230e3dd94dcd5749a959ff91cd02
TamasKerdo <kerdothamas@gmail.com>
2017-11-29 15:50:49 +0100

    feat(lists_3 is done)

---
+++ week-02/day3/Lists_3/Lists_3/Lists_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            
+            int i = 0;
+            while (i!= far.Count)
+            {
+                far[i] = far[i] + "a";
+                Console.WriteLine(far[i]);
+                i++;
+            }
+            Console.Read();
+        }
+    }
+}
