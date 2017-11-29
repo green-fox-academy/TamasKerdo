66d445c104ae5135479767fb8e28fcb1a7faf984
TamasKerdo <kerdothamas@gmail.com>
2017-11-29 16:18:11 +0100

    lists_4 is done

---
+++ week-02/day3/Lists_4/Lists_4/Lists_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+
+            
+            list[list.FindIndex(ind => ind.Equals(2))] = "Croissant";
+            list[list.FindIndex(ind => ind.Equals(false))] = "Ice cream";
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.Read();
+        }
+    }
+}
