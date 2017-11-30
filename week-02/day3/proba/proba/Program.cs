8b5f4f2901965f208bc0454520499fa7ac087169
TamasKerdo <kerdothamas@gmail.com>
2017-11-30 11:23:06 +0100

    week2 day2 dictionary1 is done

---
+++ week-02/day3/proba/proba/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Dictionary<string, int> dictionary =
+                new Dictionary<string, int>();
+
+            dictionary.Add("apple", 1);
+            dictionary.Add("windows", 5);
+
+            // See whether Dictionary contains this string.
+            if (dictionary.ContainsKey("apple"))
+            {
+                int value = dictionary["apple"];
+                Console.WriteLine(value);
+            }
+
+            // See whether it contains this string.
+            if (!dictionary.ContainsKey("acorn"))
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.Read();
+        } } }
+        
+ 
