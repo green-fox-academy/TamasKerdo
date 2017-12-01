b2c598d00825628aacda66dae2044f1f9f63ba1d
TamasKerdo <kerdothamas@gmail.com>
2017-12-01 11:59:30 +0100

    Armstron number is done

---
+++ week-02/day5/Armstrong number/Armstrong number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("give me a number:");
+            string input = Console.ReadLine();
+            char[] array = new char[input.Length];
+            array = input.ToCharArray();
+            double sum=0;
+            foreach (var number in array)
+            {
+               sum += Math.Pow((int)Char.GetNumericValue(number), array.Length);
+                               
+            }
+            Console.WriteLine(sum);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
