706f63a002773578bfec652412a476416c149067
TamasKerdo <kerdothamas@gmail.com>
2017-12-06 14:54:57 +0100

    feat(Purple steps 3D):done

---
+++ week-03/day03/Purple steps 3D/Purple steps 3D/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Purple_steps_3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            
+            int position = 0;
+            int size = 10;
+            for (int i = 0; i < 8; i++)
+            {               
+                foxDraw.FillColor(Colors.Green);
+                foxDraw.DrawRectangle(position, position, size, size);
+                
+                position = position + size;
+                size = size * (i + 2);
+
+            }
+            
+        }
+    }
+}
