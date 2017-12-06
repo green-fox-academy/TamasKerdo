8bed07c179a4882bcedcb6fdd98043dc4b9742ed
TamasKerdo <kerdothamas@gmail.com>
2017-12-06 12:00:49 +0100

    feat(square in the center):done

---
+++ week-03/day03/Square in the center/Square in the center/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Square_in_the_center
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // draw a green 10x10 square to the canvas' center.
+            
+            int width = 10;
+            Square(width);
+        }
+
+        public void Square(int width)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            int centerHeight = 525 / 2;
+            int centerWidth = 350 / 2;
+            foxDraw.StrokeColor(Colors.Red);
+                      
+            foxDraw.DrawRectangle( centerHeight+width/2,centerWidth + width / 2, width,width);
+            
+        }
+    }
+}
