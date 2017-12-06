68de9cd1d36928f068ac760a37238d504b55d34c
TamasKerdo <kerdothamas@gmail.com>
2017-12-06 10:52:24 +0100

    feat(Middle lines&Colored box):done

---
+++ week-03/day03/Colored box/Colored box/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Colored_box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            // draw a box that has different colored lines on each edge.
+
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0,0,30,0);
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(30, 0, 30, 50);
+            foxDraw.StrokeColor(Colors.Yellow);
+            foxDraw.DrawLine(30, 50, 0, 50);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 50, 0, 0);
+
+
+
+
+
+        }
+    }
+}
---
+++ week-03/day03/Middle lines/Middle lines/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Middle_lines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            //  draw a red horizontal line to the canvas' middle.
+            //  draw a green vertical line to the canvas' middle.
+
+
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 10, 100, 10);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 40, 100, 40);
+        }
+    }
+}
