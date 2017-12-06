93a1234479b824832a998dca54729f1d6d662199
TamasKerdo <kerdothamas@gmail.com>
2017-12-06 13:46:09 +0100

    feat(square positioning):done

---
+++ week-03/day03/Square positioning/square positioning/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace square_positioning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+            
+            // create a square drawing function that takes 2 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and draws a 50x50 square from that point.
+            // draw 3 squares with that function.            
+            int Xpos = 0;
+            int Ypos = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                SquareDraw(Xpos, Ypos);
+                Xpos += 40;
+                Ypos += 30;
+            }
+        }
+
+        public void SquareDraw(int Xpos, int Ypos)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.DrawRectangle(Xpos, Ypos, 50, 50);
+        }
+    }
+}
---
+++ week-03/day03/fill the reactangles/fill the reactangles/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace fill_the_reactangles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw four different size and color rectangles.
+            int xposition = 5;
+            int yposition = 10;
+            int height = 50;
+            int width = 10;
+            int color = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                xposition += 10;
+                yposition += 20;
+                height += 10;
+                width += 5;               
+                Reactangle(xposition, yposition, width, height, color);
+                color++;
+            }
+
+        }
+
+        public void Reactangle(int xposition, int yposition, int width, int height, int color)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            switch (color)
+            {
+                case 0:
+
+                    foxDraw.FillColor(Colors.Green);
+                    foxDraw.DrawRectangle(xposition, yposition, width, height);
+                    break;
+                case 1:
+
+                    foxDraw.FillColor(Colors.Red);
+                    foxDraw.DrawRectangle(xposition, yposition, width, height);
+                    break;
+                case 2:
+
+                    foxDraw.FillColor(Colors.Blue);
+                    foxDraw.DrawRectangle(xposition, yposition, width, height);
+                    break;
+                case 3:
+
+                    foxDraw.FillColor(Colors.Yellow);
+                    foxDraw.DrawRectangle(xposition, yposition, width, height);
+                    break;
+            }
+        }
+    }
+}
