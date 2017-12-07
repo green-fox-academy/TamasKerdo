72e89dfc7a98da93d2fa6369c57af2604845654c
TamasKerdo <kerdothamas@gmail.com>
2017-12-07 12:22:29 +0100

    feat(07String):done

---
+++ week-03/day04/06Bunnies again/Bunnies again/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnies_again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+            int bunnieNumber = 10;   
+            
+            Console.WriteLine(BunnieCounter(bunnieNumber));
+            Console.ReadLine();
+
+        }
+
+        static int BunnieCounter(int bunnieNumber)
+        {
+            if (bunnieNumber == 1)
+            {
+                Console.WriteLine(1);
+                return 2;
+
+            }
+            else if (bunnieNumber % 2 == 0)
+            {
+                Console.WriteLine(bunnieNumber);
+                return 3 + BunnieCounter(--bunnieNumber);
+            }
+            else 
+            {
+                Console.WriteLine(bunnieNumber);
+                return 2 + BunnieCounter(--bunnieNumber);
+            }
+            
+        }
+    }
+}
---
+++ week-03/day04/0707Strings/07Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+
+            Console.WriteLine(StringChanger("zxcvbnm,zxcvbnmzxcvbnmzxcvbnm"));
+            Console.ReadLine();
+
+        }
+
+        static string StringChanger(string word)
+        {
+            var chars = word.ToCharArray();
+            if (chars.Contains('x'))
+            {
+                int index = Array.IndexOf(chars, 'x');
+                chars[index] = 'y';
+                string modifiedWord = new string(chars);
+                return StringChanger(modifiedWord);
+            }
+            else
+            {
+                return word;
+            }
+            
+        }
+    }
+}
