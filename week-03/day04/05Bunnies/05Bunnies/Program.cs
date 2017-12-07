55dce05a622d5cacad5c7d69c5027b5354d21ff2
TamasKerdo <kerdothamas@gmail.com>
2017-12-07 11:24:49 +0100

    feat(bunnyCounter):done

---
+++ week-03/day04/05Bunnies/05Bunnies/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+            int bunnyNumber = 30;
+            int earNumber = 0;
+            
+            Console.WriteLine(BunnieCounter(bunnyNumber, earNumber));
+            Console.ReadLine();
+        }
+
+        static int BunnieCounter(int bunnyNumber, int earNumber)
+        {
+
+            if (bunnyNumber > 0)
+            {
+
+                return BunnieCounter(--bunnyNumber, earNumber + 2);
+            }
+            else
+            {
+                return earNumber;
+            }
+        }
+    }
+}
