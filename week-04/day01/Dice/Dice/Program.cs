71845218ebd669ab57b7b4a828aae1985d12df63
TamasKerdo <kerdothamas@gmail.com>
2017-12-11 21:25:28 +0100

    feat(dice):done

---
+++ week-04/day01/Dice/Dice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SixDice Dice = new SixDice();
+            bool dicesAllSix = false;
+            int RollCounter = 0;
+            while (dicesAllSix != true)
+            {
+                Dice.Roll();
+                int[] array = new int[6];
+                if (Dice.GetDices().Contains(1) || Dice.GetDices().Contains(2) || Dice.GetDices().Contains(3) || Dice.GetDices().Contains(4) || Dice.GetDices().Contains(5))
+                {
+                    dicesAllSix = false;
+                    RollCounter++;                    
+                }
+                else
+                {
+                    dicesAllSix = true;                    
+                }  
+
+            }
+
+            Console.WriteLine(RollCounter);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-04/day01/Dice/Dice/SixDice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class SixDice
+    {
+        private static int[] Dices = new int[6];
+             
+
+        public void Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Random rn = new Random();
+                Dices[i] = rn.Next(1, 7);
+            }
+            
+        }
+        public int[] GetDices()
+        {
+            return Dices;
+        }
+    }
+}
