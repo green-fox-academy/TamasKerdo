784dabf28830712a52eb084c4d07c22277dda10c
TamasKerdo <kerdothamas@gmail.com>
2017-12-12 12:26:20 +0100

    feat(Greenfox inharitance exercise):Student class is created

---
+++ week-04/day02/Green Fox inheritance exercise/Green Fox inheritance exercise/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_inheritance_exercise
+{
+    class Student
+    {
+        //Create a Student class that has the same fields and methods as the Person class, and has the following additional
+
+        //fields:
+        //previousOrganization: the name of the student’s previous company / school
+        //skippedDays: the number of days skipped from the course
+        //methods:
+        //GetGoal(): prints out "Be a junior software developer."
+        //Introduce(): "Hi, I'm name, a age year old gender from previousOrganization who skipped skippedDays days from the course already."
+        //SkipDays(numberOfDays): increases skippedDays by numberOfDays
+        //The Student class has the following constructors:
+
+        //Student(name, age, gender, previousOrganization) : beside the given parameters, it sets skippedDays to 0
+        //Student() : sets name to Jane Doe, age to 30, gender to female, previousOrganization to The School of Life, skippedDays to 0
+    }
+}
