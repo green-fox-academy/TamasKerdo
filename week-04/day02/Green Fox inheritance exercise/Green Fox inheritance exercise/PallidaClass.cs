a0e1208b32ef159c40e93787c3ed463b4d2f7683
TamasKerdo <kerdothamas@gmail.com>
2017-12-12 12:21:14 +0100

    feat(Greenfox inharitance exercise):PallidaClass is created

---
+++ week-04/day02/Green Fox inheritance exercise/Green Fox inheritance exercise/PallidaClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Fox_inheritance_exercise
+{
+    class PallidaClass
+    {
+        //Create a PallidaClass class that has the following
+
+        //fields:
+        //className: the name of the class
+        //students : a list of Students
+        //mentors: a list of Mentors
+        //methods:
+        //AddStudent(Student): adds the given Student to students list
+        //AddMentor(Mentor): adds the given Mentor to mentors list
+        //Info(): prints out "Pallida className class has students.size students and mentors.size mentors."
+        //The PallidaClass class has the following constructors:
+
+        //PallidaClass(className) : beside the given parameter, it sets students and mentors as empty lists
+    }
+}
