e8056f3884f68a834e7c7ff3d21df5df33ff8bfa
TamasKerdo <kerdothamas@gmail.com>
2017-12-19 10:12:49 +0100

    feat(RPG_Game)_caracter class is added

---
+++ week-05/RPG_Game/RPG_Game/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Game
+{
+    class Character
+    {
+        public int HP { get; private set; }
+        public int DP { get; private set; }
+        public int SP { get; private set; }
+
+        public Character()
+            {
+
+            }
+
+    }
+}
