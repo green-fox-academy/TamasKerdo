fc6f43bdc8177c2a61091d1207804713a508c27f
TamasKerdo <kerdothamas@gmail.com>
2018-01-02 17:18:15 +0100

    feat(reservations): finished

---
+++ week-06/day1/Reservations/Reservations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reservations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = new Reservation();
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.WriteLine($"Booking# {res.GetCodeBooking()} for {res.GetDowBooking()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/day1/Reservations/Reservations/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservations
+{
+    class Reservation : IReservationy
+    {     
+        public string GetCodeBooking()
+        {
+            Random rand = new Random();
+
+            string Alphabet =
+            "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        
+            char[] chars = new char[8];
+            for (int i = 0; i < 8; i++)
+            {
+                chars[i] = Alphabet[rand.Next(Alphabet.Length)];
+            }
+            string s = new string(chars);
+            return s;
+        }
+
+        public string GetDowBooking()
+        {
+            Random rand = new Random();
+            string[] daysOfTheWeek = new string[] { "mon", "tue", "wed", "thi", "fri", "sat", "sun" };
+
+            return daysOfTheWeek[rand.Next(0, 7)];
+        }
+    }
+}
+
+
