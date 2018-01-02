deb4480b2ff8a337d5c6d1667c952a1eeed54d62
TamasKerdo <kerdothamas@gmail.com>
2018-01-02 22:46:20 +0100

    feat(homepractice_2):finished

---
+++ week-06/day2/homepractice/2/two/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace two
+{
+    delegate double doubleIt(double val);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doubleIt dblIt = x => x * 2;
+            Console.WriteLine($"5*2 = {dblIt(5)}");
+
+            List<int> numlist = new List<int> { 1, 9, 2, 6, 3 };
+
+            var evenList = numlist.Where(a => a % 2 == 0).ToList();
+            foreach (var j in evenList)            
+                Console.WriteLine(j);
+
+            var rangeList = numlist.Where(x => (x > 2) && (x < 9)).ToList();
+
+            foreach (var j in rangeList)
+                Console.WriteLine(j);
+
+            Console.ReadLine();
+        }
+    }
+}
