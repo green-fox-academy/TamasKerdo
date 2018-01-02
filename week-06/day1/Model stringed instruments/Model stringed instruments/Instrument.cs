2fceb37c9bd190e921b86925db662eb2e199a364
TamasKerdo <kerdothamas@gmail.com>
2018-01-02 14:04:14 +0100

    feat(Model stringed instruments): project is created

---
+++ week-06/day1/Model stringed instruments/Model stringed instruments/Instrument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model_stringed_instruments
+{
+    abstract class Instrument
+    {
+        //We start with a base, abstract class Instrument.
+
+        //it reserves(e.g. protected) the name of the instrument
+        //it should provide a Play() method.
+
+        protected string Name { get; set; }
+
+        abstract public void Play();
+    }
+}
---
+++ week-06/day1/Model stringed instruments/Model stringed instruments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model_stringed_instruments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
