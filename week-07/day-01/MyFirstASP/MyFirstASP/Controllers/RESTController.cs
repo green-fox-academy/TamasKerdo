75526e3b177e24724b0669622d768ce1e30c0465
TamasKerdo <kerdothamas@gmail.com>
2018-01-08 14:11:18 +0100

    feat(MyFirstASP):RESTController is added

---
+++ week-07/day-01/MyFirstASP/MyFirstASP/Controllers/RESTController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASP.Controllers
+{
+    public class RESTController : Controller
+    {
+        [Route("api")]
+        public IActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var v = new { id = 1, content = "Hello,World" };
+            return new JsonResult(v);
+        }
+    }
+}
+
