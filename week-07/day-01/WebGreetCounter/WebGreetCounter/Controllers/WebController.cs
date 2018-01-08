fc71cccf6b53c8980f48d1d9a071b59e219bceb3
TamasKerdo <kerdothamas@gmail.com>
2018-01-08 15:56:44 +0100

    feat(WebGreetCounter):project finished

---
+++ week-07/day-01/WebGreetCounter/WebGreetCounter/Controllers/WebController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebGreetCounter.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebGreetCounter.Controllers
+{    
+
+    [Route("web")]
+    public class WebController : Controller
+    {
+        public static int Counter { get; set; }
+
+        // GET: /<controller>/
+        [Route("greeting")]
+        public IActionResult Greeting([FromQuery] string name)
+        {
+            var greeting = new Greeting();
+            greeting.Content = "Hello"+ name;
+            greeting.Id = Counter;
+            Counter++;
+
+            return new JsonResult(greeting);
+        }
+    }
+}
