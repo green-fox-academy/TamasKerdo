630c701776a814e6d41d8ca51a7241b5f3e5650b
TamasKerdo <kerdothamas@gmail.com>
2018-01-10 23:10:18 +0100

    feat(GameForDemo):generateImageList of ImageViewModel is ready

--- week-07/GameForDemo/GameForDemo/ViewModel/ImageViewModel.cs
+++ week-07/GameForDemo/GameForDemo/ViewModel/ImageViewModel.cs
@@ -10,5 +10,29 @@ namespace GameForDemo.ViewModel
     {
         public List<HackerImage> ImageList { get; set; }
         public List<StartImage> StartImageList { get; set; }
+
+        public void GenerateImageList()
+        {
+            var linkList = new List<string>();
+            linkList[0] = @"D:\Greenfox Academy tananyag\practice\week07\day3\images\admin.jpg";
+            linkList[1] = @"D:\Greenfox Academy tananyag\practice\week07\day3\images\geek.jpg";
+            linkList[2] = @"D:\Greenfox Academy tananyag\practice\week07\day3\images\hacker.jpg";
+
+            var rn = new Random();
+            int hackerNumber=0;
+            for (int i = 0; i < 3; i++)
+            {                
+                int generatedRandomNumber = rn.Next(0, linkList.Count);
+                ImageList.Add(new HackerImage());
+                ImageList[i].ImageLink = linkList[generatedRandomNumber];
+                ImageList[i].IsThisAHacker = false;
+
+                if (i == 0)
+                {
+                    hackerNumber = generatedRandomNumber;
+                }
+            }
+            ImageList[hackerNumber].IsThisAHacker = true;            
+        }
     }
 }
