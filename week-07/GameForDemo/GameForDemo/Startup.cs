ea180a0542c0ad683e68b7c0f794aae28fd8760b
TamasKerdo <kerdothamas@gmail.com>
2018-01-10 21:49:52 +0100

    feat(GameForDemo):imageViewModels class is cerated

--- week-07/GameForDemo/GameForDemo/Startup.cs
+++ week-07/GameForDemo/GameForDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace GameForDemo
 {
     public class Startup
@@ -25,7 +26,8 @@ namespace GameForDemo
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
+            app.UseMvc();
         }
     }
 }
---
+++ week-07/GameForDemo/GameForDemo/ViewModel/ImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameForDemo.Models;
+
+namespace GameForDemo.ViewModel
+{
+    public class ImageViewModel
+    {
+        public List<Image> ImageList { get; set; } = new List<Image>();
+    }
+}
