69e257235a1fbe6a8db11f53947e4e564ec54e41
TamasKerdo <kerdothamas@gmail.com>
2018-01-10 13:09:25 +0100

    feat(Bank of simba):BankAccounts in Models created

---
+++ week-07/day-03/BankOfSimba/BankOfSimba/Controllers/SimbaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    public class SimbaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var ba = new BankAccounts() {Name = "Simba", Balance = 2000, AnimalType = "Animal.Lion" };
+            return View(ba);
+        }
+    }
+}
