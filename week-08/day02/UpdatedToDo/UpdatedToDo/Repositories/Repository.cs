9bb61921f176a03f4082f89da9b7d8128c95d76f
TamasKerdo <kerdothamas@gmail.com>
2018-01-16 20:35:32 +0100

    feat(UpdatedToDo):Repositories folder and Repository class is created

---
+++ week-08/day02/UpdatedToDo/UpdatedToDo/Repositories/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpdatedToDo.Entities;
+
+namespace UpdatedToDo.Repositories
+{
+    public class Repository
+    {
+        public ToDoContext ToDoContext { get; set; }
+
+        public Repository(ToDoContext ToDoContext)
+        {
+            this.ToDoContext = ToDoContext;
+        }
+    }
+}
