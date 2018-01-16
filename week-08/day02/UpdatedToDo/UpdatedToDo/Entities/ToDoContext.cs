ce67e881e35daf4a682782f9b1920a72cfab64de
TamasKerdo <kerdothamas@gmail.com>
2018-01-16 20:29:13 +0100

    feat(UpdatedToDo):Entities & Controllers folders are created

---
+++ week-08/day02/UpdatedToDo/UpdatedToDo/Entities/ToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UpdatedToDo.Models;
+
+namespace UpdatedToDo.Entities
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
---
+++ week-08/day02/UpdatedToDo/UpdatedToDo/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpdatedToDo.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public bool isDone { get; set; }
+        public bool isUrgent { get; set; }
+    }
+}
