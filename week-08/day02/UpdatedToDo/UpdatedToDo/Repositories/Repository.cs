d1f059855255da69c648154ca47bdfa203f02c03
TamasKerdo <kerdothamas@gmail.com>
2018-01-16 21:43:20 +0100

    feat(UpdatedToDo):3 list and add method are added in repository.cs

--- week-08/day02/UpdatedToDo/UpdatedToDo/Repositories/Repository.cs
+++ week-08/day02/UpdatedToDo/UpdatedToDo/Repositories/Repository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UpdatedToDo.Entities;
+using UpdatedToDo.Models;
 
 namespace UpdatedToDo.Repositories
 {
@@ -13,6 +14,30 @@ namespace UpdatedToDo.Repositories
         public Repository(ToDoContext ToDoContext)
         {
             this.ToDoContext = ToDoContext;
+        }
+
+        public void AddNewToDo(ToDo newTodo)
+        {
+            ToDo todo = newTodo;
+            ToDoContext.ToDos.Add(todo);            
+        }
+
+        public List<ToDo> ListAllToDo()
+        {
+            var todoList = ToDoContext.ToDos.ToList();
+            return todoList;
+        }
+
+        public List<ToDo> ListFinishedToDos()
+        {
+            var todoList = ToDoContext.ToDos.Where(x=>x.isDone== true).ToList();
+            return todoList;
+        }
+
+        public List<ToDo> ListUnFinishedToDos()
+        {
+            var todoList = ToDoContext.ToDos.Where(x => x.isDone == false).ToList();
+            return todoList;
         }
     }
 }
