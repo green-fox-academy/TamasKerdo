f60eb09e6c2f1d4755fe248b9e0fbe827df53c79
TamasKerdo <kerdothamas@gmail.com>
2018-01-16 20:48:17 +0100

    fix(UpdatedToDo):services.adddbcontext is fixed

--- week-08/day02/UpdatedToDo/UpdatedToDo/Startup.cs
+++ week-08/day02/UpdatedToDo/UpdatedToDo/Startup.cs
@@ -19,7 +19,7 @@ namespace UpdatedToDo
         {
             services.AddMvc();
             services.AddScoped<Repository>();
-            services.AddScoped<ToDoContext>();
+            services.AddDbContext<ToDoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
