462b89637d19640a6628390e824076324de40ed1
TamasKerdo <kerdothamas@gmail.com>
2018-01-18 10:40:49 +0100

    fix(reddit):Entities, Repositories

--- week-08/Reddit/Reddit/Controllers/HomeController.cs
+++ week-08/Reddit/Reddit/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Reddit.Repositories;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +11,13 @@ namespace Reddit.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        public Repository Repo { get; set; }
+
+        public HomeController()
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ week-08/Reddit/Reddit/Repositories/Repository.cs
@@ -0,0 +1,18 @@
+﻿using Reddit.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Repositories
+{
+    public class Repository
+    {
+        public PostContext PC { get; set; }
+
+        public Repository(PostContext postContext)
+        {
+            PC = postContext;
+        }
+    }
+}
