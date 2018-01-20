bea451119dba9ee1e0ef8988accf7a992f50726e
TamasKerdo <kerdothamas@gmail.com>
2018-01-20 16:42:35 +0100

    feat(FirstSQLServerSetup): folder deleted

--- week-08/day01/FirstSQLServerSetup/something/dbo.ToDos.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[ToDos] (
-    [Id]       BIGINT         IDENTITY (1, 1) NOT NULL,
-    [IsDone]   BIT            NOT NULL,
-    [IsUrgent] BIT            NOT NULL,
-    [Title]    NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_ToDos] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
