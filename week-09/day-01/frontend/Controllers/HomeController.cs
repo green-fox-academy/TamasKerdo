060c56f772c4ca3a7e7de7fde705323553bc9582
TamasKerdo <kerdothamas@gmail.com>
2018-01-22 13:53:56 +0100

    fix(dountil): finished

--- week-09/day-01/frontend/Controllers/HomeController.cs
+++ week-09/day-01/frontend/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using frontend.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -49,6 +50,37 @@ namespace frontend.Controllers
         {
             var result = new NotFoundObjectResult(new { message = "404 Not Found", currentDate = DateTime.Now });
             return result;
+        }
+
+        [HttpPost("dountil/{what}")]
+        public IActionResult Dountil([FromRoute] string what, [FromBody] Item input)
+        {
+            if (input.until !=null)
+            {
+                switch (what)
+                {
+
+                    case "sum":
+                        int sum = 0;
+                        for (int i = 1; i < input.until+1; i++)
+                        {
+                            sum += i;
+                        }
+                        return Json(new { result = sum });
+                        
+                    case "factor":
+                        int factorial = 1;
+                        for (int i = 1; i < input.until+1; i++)
+                        {
+                            factorial *= i;
+                        }
+                        return Json(new { result = factorial });
+                        
+                    default:
+                        break;
+                }
+            }
+            return Json(new { error = "Please provide a number!" });
         }
     }
 }
