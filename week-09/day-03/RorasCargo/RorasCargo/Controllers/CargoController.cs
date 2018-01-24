d376b73c5a37b33c0e36ecf57754eb41007ead53
TamasKerdo <kerdothamas@gmail.com>
2018-01-24 16:10:41 +0100

    feat(RorasCargo): project is created

---
+++ week-09/day-03/RorasCargo/RorasCargo/Controllers/CargoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RorasCargo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RorasCargo.Controllers
+{
+    [Route("")]
+    public class CargoController : Controller
+    {
+        public Cargo cargo { get; set; }
+
+        public CargoController(Cargo cargo)
+        {
+            this.cargo = cargo;
+        }
+        
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpGet("rocket")]
+        public IActionResult CargoList()
+        {
+
+        }
+    }
+}
---
+++ week-09/day-03/RorasCargo/RorasCargo/Models/Cargo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RorasCargo.Models
+{
+    public class Cargo
+    {
+        public int caliber25 { get; set; }
+        public int caliber30 { get; set; }
+        public int caliber50 { get; set; }
+        public string IsEmpty { get; set; }
+        public bool Ready { get; set; }
+    }
+}
