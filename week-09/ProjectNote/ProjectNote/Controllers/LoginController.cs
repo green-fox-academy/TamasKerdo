796d596a285bc42bbccfb31963a9383d3a832e6e
TamasKerdo <kerdothamas@gmail.com>
2018-01-24 23:17:27 +0100

    feat(ProjectNote):CreateUser & VerifyPassword in LoginController

---
+++ week-09/ProjectNote/ProjectNote/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectNote.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectNote.Controllers
+{
+    [Route("")]
+    public class LoginController : Controller
+    {
+        public Repository repository { get; set; }
+
+        public LoginController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+        [HttpPost("CreateUser")]
+        public IActionResult CreateUser([FromQuery] string name,[FromQuery] string password,[FromQuery] string greenfoxClass)
+        {
+            repository.AddNewUser();
+            return View();
+        }
+
+        [HttpGet("VerifyPassword")]
+        public IActionResult VerifyPassword([FromQuery] string name, [FromQuery] string password)
+        {
+            return Ok();
+        }
+    }
+}
