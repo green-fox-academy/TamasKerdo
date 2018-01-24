dc1aaca1210d9fdced2087b87be7c2a8e89c1854
TamasKerdo <kerdothamas@gmail.com>
2018-01-25 00:04:21 +0100

    feat(ProjectNote):addnewuser method is finished in repository

--- week-09/ProjectNote/ProjectNote/Repositories/Repository.cs
+++ week-09/ProjectNote/ProjectNote/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using ProjectNote.Models;
+﻿using ProjectNote.Entities;
+using ProjectNote.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,21 @@ namespace ProjectNote.Repositories
 {
     public class Repository
     {
-        internal void AddNewUser(string name,string password,string greenfoxClass)
+        public ProjectNoteContext PNC { get; set; }
+
+        public Repository(ProjectNoteContext PNC)
+        {
+            this.PNC = PNC; 
+        }
+
+        public void AddNewUser(string name,string password,string greenfoxClass)
         {
             User newUser = new User();
             newUser.name = name;
             newUser.password = password;
             newUser.greenfoxClass = greenfoxClass;
+            PNC.users.Add(newUser);
+            PNC.SaveChanges();
         }
     }
 }
