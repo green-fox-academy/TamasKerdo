eab4d2252a6702a11602d58661532d2f383b0197
TamasKerdo <kerdothamas@gmail.com>
2018-01-24 23:21:48 +0100

    feat(ProjectNote):Project class and user class are finished

--- week-09/ProjectNote/ProjectNote/Models/Project.cs
+++ week-09/ProjectNote/ProjectNote/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace ProjectNote.Models
 {
     public class Project
     {
+        [Key]
+        public long? projectId { get; set; }
+        public string link { get; set; }
+        public string description { get; set; }
+        public string programmingLanguage { get; set; }
     }
 }
--- week-09/ProjectNote/ProjectNote/Models/User.cs
+++ week-09/ProjectNote/ProjectNote/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace ProjectNote.Models
 {
     public class User
     {
+        [Key]
+        public long? UserId { get; set; }
+        public string name { get; set; }
+        public string greenfoxClass { get; set; }
     }
 }
