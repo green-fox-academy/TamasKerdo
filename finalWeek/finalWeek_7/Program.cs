fe648755266ac3e747607b23a15d1764cceba4fb
TamasKerdo <kerdothamas@gmail.com>
2018-03-26 09:28:13 +0200

    feat(LetterFinder): LetterFinder method is finsihed

---
+++ finalWeek/finalWeek_7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace finalWeek_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public List<int> LetterFinder(string inputWord, char letterToFind)
+        {
+            var FoundLetters = new List<int>();
+            var inputWordArray = new List<char>();
+            inputWordArray = inputWord.ToCharArray().ToList();
+
+            for (int i = 0; i < inputWordArray.Count; i++)
+            {
+                if (inputWordArray[i].Equals(letterToFind))
+                {
+                    FoundLetters.Add(i);
+                }
+            }
+
+            return FoundLetters();
+        }
+    }
+}
